53bafa49f02d099168cfed4608b1e04e70ff251d
Bryan Worrell <bworrell@mitre.org>
2014-08-27 17:48:58 -0400

    Added NSMAP dictionaries to the top of each STIXUpdater class. Began work on clean() method in STIX_1_0_Updater.

--- ramrod/update/stix/update.py
+++ ramrod/update/stix/update.py
@@ -1,14 +1,42 @@
+import copy
+from collections import
+
 from ramrod.update import (UnknownVersionException,
     UntranslatableFieldException, UpdateException, IncorrectVersionException)
 
 STIX_VERSIONS = ('1.0', '1.0.1', '1.1', '1.1.1')
 
 class STIX_1_0_Updater(object):
+    NSMAP = {'http://data-marking.mitre.org/Marking-1': 'marking',
+             'http://data-marking.mitre.org/extensions/MarkingStructure#Simple-1': 'simpleMarking',
+             'http://data-marking.mitre.org/extensions/MarkingStructure#TLP-1': 'tlpMarking',
+             'http://stix.mitre.org/Campaign-1': 'campaign',
+             'http://stix.mitre.org/CourseOfAction-1': 'coa',
+             'http://stix.mitre.org/ExploitTarget-1': 'et',
+             'http://stix.mitre.org/Incident-1': 'incident',
+             'http://stix.mitre.org/Indicator-2': 'indicator',
+             'http://stix.mitre.org/TTP-1': 'ttp',
+             'http://stix.mitre.org/ThreatActor-1': 'ta',
+             'http://stix.mitre.org/common-1': 'stixCommon',
+             'http://stix.mitre.org/default_vocabularies-1': 'stixVocabs',
+             'http://stix.mitre.org/extensions/AP#CAPEC2.5-1': 'capecAP',
+             'http://stix.mitre.org/extensions/Address#CIQAddress3.0-1': 'ciqAddress',
+             'http://stix.mitre.org/extensions/Identity#CIQIdentity3.0-1': 'ciqIdentity',
+             'http://stix.mitre.org/extensions/Malware#MAEC4.0-1': 'maecMalware',
+             'http://stix.mitre.org/extensions/StructuredCOA#Generic-1': 'genericStructuredCOA',
+             'http://stix.mitre.org/extensions/TestMechanism#Generic-1': 'genericTM',
+             'http://stix.mitre.org/extensions/TestMechanism#OVAL5.10-1': 'ovalTM',
+             'http://stix.mitre.org/extensions/TestMechanism#OpenIOC2010-1': 'openiocTM',
+             'http://stix.mitre.org/extensions/TestMechanism#Snort-1': 'snortTM',
+             'http://stix.mitre.org/extensions/TestMechanism#YARA-1': 'yaraTM',
+             'http://stix.mitre.org/extensions/Vulnerability#CVRF-1': 'vulnCVRF',
+             'http://stix.mitre.org/stix-1': 'stix'}
+
     def __init__(self):
         pass
 
 
-    def can_update(self, root):
+    def can_update(self, root, remove=False):
         """Determines if the input document can be upgraded from STIX v1.0 to
         STIX v1.0.1.
 
@@ -55,11 +83,51 @@ class STIX_1_0_Updater(object):
             from the input document.
 
         """
-        pass
+        removed = []
+        nsmap = {"xsi":  "http://www.w3.org/2001/XMLSchema-instance"}
+        xpath = "//*[@xsi:type]"
+        nodes = root.xpath(xpath, namespaces=nsmap)
+
+        disallowed = ("MAEC4.0InstanceType", "CAPEC2.5InstanceType",
+                      "CIQIdentity3.0InstanceType")
+
+        for node in nodes:
+            xsi_type = node.attrib["{http://www.w3.org/2001/XMLSchema-instance}type"]
+            type_ = xsi_type.split(":")[1]
+
+            if type_ in disallowed:
+                dup = copy.deepcopy(node)
+                removed.append(dup)
+                parent = node.getparent()
+                parent.remove(node)
+
+        return removed
 
 
     def update(self, root, force=False):
-        """Attempts to update an input STIX v1.0 document to STIX v1.0.1
+        """Attempts to update an input STIX v1.0 document to STIX v1.0.1.
+
+        This method performs the following changes:
+        * Removes schemaLocations
+        * STIX_Package/@version 1.0 => 1.0.1
+        * MotivationVocab 1.0 => 1.0.1
+          * "Ideological - Anti-Establisment" => "Ideological - Anti-Establishment"
+        * PlanningAndOperationalSupportVocab 1.0 => 1.0.1
+          * "Planning " => "Planning"
+          * "Planning - Open-Source Intelligence (OSINT) Gethering" => "Planning - Open-Source Intelligence (OSINT) Gathering"
+        * Threat_Actor/@version 1.0 => 1.0.1
+        * TTP/@version 1.0 => 1.0.1
+        * Campaign/@version 1.0 => 1.0.1
+        * COA/@version 1.0 => 1.0.1
+        * Incident/@version 1.0 => 1.0.1
+        * Indicator/@version 2.0 => 2.0.1
+        * Campaign/@version 1.0 => 1.0.1
+        * Exploit Target@version 1.0 => 1.0.1
+
+        Untranslatable items:
+        * MAEC 4.0 Malware extension
+        * CAPEC 2.5 Attack Pattern extension
+        * CIQ Identity 3.0 Extension
 
         Args:
             root (lxml.etree._Element): The top-level node of the STIX
@@ -78,10 +146,63 @@ class STIX_1_0_Updater(object):
                 input document.
 
         """
-        pass
+
+        xpath_versions = ("//indicator:Indicator[@version] | "
+                          "stix:Indicator[@version] | "
+                          "stixCommon:Indicator[@version] | "
+                          "incident:Incident[@version] | "
+                          "stix:Incident[@version] | "
+                          "stixCommon:Incident[@version] | "
+                          "ttp:TTP[@version] | "
+                          "stix:TTP[@version] | "
+                          "stixCommon:TTP[@version] | "
+                          "coa:Course_Of_Action[@version] | "
+                          "stix:Course_Of_Action[@version] | "
+                          "stixCommon:Course_Of_Action[@version] |"
+                          "ta:Threat_Actor[@version]| "
+                          "stix:Threat_Actor[@version] | "
+                          "stixCommon:Threat_Actor[@version] | "
+                          "campaign:Campaign[@version] | "
+                          "stix:Campaign[@version] | "
+                          "stixCommon:Campaign[@version] | "
+                          "et:Exploit_Target[@version] | "
+                          "stix:Exploit_Target[@version] | "
+                          "stixCommon:Exploit_Target[@version]")
+
+        nodes = root.xpath(xpath_versions, namespaces=nsmap)
+
+
+
+
+
 
 
 class STIX_1_0_1_Updater(object):
+    NSMAP = {'http://data-marking.mitre.org/Marking-1': 'marking',
+             'http://data-marking.mitre.org/extensions/MarkingStructure#Simple-1': 'simpleMarking',
+             'http://data-marking.mitre.org/extensions/MarkingStructure#TLP-1': 'tlpMarking',
+             'http://stix.mitre.org/Campaign-1': 'campaign',
+             'http://stix.mitre.org/CourseOfAction-1': 'coa',
+             'http://stix.mitre.org/ExploitTarget-1': 'et',
+             'http://stix.mitre.org/Incident-1': 'incident',
+             'http://stix.mitre.org/Indicator-2': 'indicator',
+             'http://stix.mitre.org/TTP-1': 'ttp',
+             'http://stix.mitre.org/ThreatActor-1': 'ta',
+             'http://stix.mitre.org/common-1': 'stixCommon',
+             'http://stix.mitre.org/default_vocabularies-1': 'stixVocabs',
+             'http://stix.mitre.org/extensions/AP#CAPEC2.6-1': 'capecAP',
+             'http://stix.mitre.org/extensions/Address#CIQAddress3.0-1': 'ciqAddress',
+             'http://stix.mitre.org/extensions/Identity#CIQIdentity3.0-1': 'stix-ciq',
+             'http://stix.mitre.org/extensions/Malware#MAEC4.0-1': 'maecMalware',
+             'http://stix.mitre.org/extensions/StructuredCOA#Generic-1': 'genericStructuredCOA',
+             'http://stix.mitre.org/extensions/TestMechanism#Generic-1': 'genericTM',
+             'http://stix.mitre.org/extensions/TestMechanism#OVAL5.10-1': 'ovalTM',
+             'http://stix.mitre.org/extensions/TestMechanism#OpenIOC2010-1': 'openiocTM',
+             'http://stix.mitre.org/extensions/TestMechanism#Snort-1': 'snortTM',
+             'http://stix.mitre.org/extensions/TestMechanism#YARA-1': 'yaraTM',
+             'http://stix.mitre.org/extensions/Vulnerability#CVRF-1': 'vulnCVRF',
+             'http://stix.mitre.org/stix-1': 'stix'}
+
     def __init__(self):
         pass
 
@@ -113,6 +234,32 @@ class STIX_1_0_1_Updater(object):
 
 
 class STIX_1_1_Updater(object):
+    NSMAP = {'http://data-marking.mitre.org/Marking-1': 'marking',
+             'http://data-marking.mitre.org/extensions/MarkingStructure#Simple-1': 'simpleMarking',
+             'http://data-marking.mitre.org/extensions/MarkingStructure#TLP-1': 'tlpMarking',
+             'http://data-marking.mitre.org/extensions/MarkingStructure#Terms_Of_Use-1': 'TOUMarking',
+             'http://stix.mitre.org/Campaign-1': 'campaign',
+             'http://stix.mitre.org/CourseOfAction-1': 'coa',
+             'http://stix.mitre.org/ExploitTarget-1': 'et',
+             'http://stix.mitre.org/Incident-1': 'incident',
+             'http://stix.mitre.org/Indicator-2': 'indicator',
+             'http://stix.mitre.org/TTP-1': 'ttp',
+             'http://stix.mitre.org/ThreatActor-1': 'ta',
+             'http://stix.mitre.org/common-1': 'stixCommon',
+             'http://stix.mitre.org/default_vocabularies-1': 'stixVocabs',
+             'http://stix.mitre.org/extensions/AP#CAPEC2.7-1': 'stix-capec',
+             'http://stix.mitre.org/extensions/Address#CIQAddress3.0-1': 'stix-ciqaddress',
+             'http://stix.mitre.org/extensions/Identity#CIQIdentity3.0-1': 'stix-ciqidentity',
+             'http://stix.mitre.org/extensions/Malware#MAEC4.1-1': 'stix-maec',
+             'http://stix.mitre.org/extensions/StructuredCOA#Generic-1': 'genericStructuredCOA',
+             'http://stix.mitre.org/extensions/TestMechanism#Generic-1': 'genericTM',
+             'http://stix.mitre.org/extensions/TestMechanism#OVAL5.10-1': 'stix-oval',
+             'http://stix.mitre.org/extensions/TestMechanism#OpenIOC2010-1': 'stix-openioc',
+             'http://stix.mitre.org/extensions/TestMechanism#Snort-1': 'snortTM',
+             'http://stix.mitre.org/extensions/TestMechanism#YARA-1': 'yaraTM',
+             'http://stix.mitre.org/extensions/Vulnerability#CVRF-1': 'stix-cvrf',
+             'http://stix.mitre.org/stix-1': 'stix'}
+
     def __init__(self):
         pass
 
