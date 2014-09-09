c129c3b2c73966907088b962bb64dfd990aebf27
Bryan Worrell <bworrell@mitre.org>
2014-09-09 16:25:32 -0400

    Added methods to modify and remove namespaces/schemalocations from input documents.

--- ramrod/__init__.py
+++ ramrod/__init__.py
@@ -35,6 +35,13 @@ class InvalidVersionError(Exception):
 
 
 class _BaseUpdater(object):
+
+    # OVERRIDE THESE IN IMPLEMENTATIONS
+    DISALLOWED_NAMESPACES = ()
+    NSMAP = {}
+    UPDATE_NS_MAP = {}
+    UPDATE_SCHEMALOC_MAP = {}
+
     def __init__(self):
         pass
 
@@ -54,6 +61,7 @@ class _BaseUpdater(object):
         """
         return root.nsmap.get(ns)
 
+
     def _remove_xml_node(self, node):
         parent = node.getparent()
         parent.remove(node)
@@ -69,6 +77,83 @@ class _BaseUpdater(object):
         except KeyError:
             # Attribute was not found
             pass
+
+
+    def _remove_schemalocations(self, root):
+        self._remove_xml_attribute(root, TAG_SCHEMALOCATION)
+
+
+    def _clean_schemalocs(self, pairs):
+        cleaned = []
+        for ns, loc in pairs:
+            if ns not in self.DISALLOWED_NAMESPACES:
+                cleaned.append((ns,loc))
+
+        return cleaned
+
+
+    def _create_schemaloc_str(self, pairs):
+        schemaloc_str = "   ".join(("%s %s" % (ns, loc)) for ns, loc in pairs)
+        return schemaloc_str
+
+
+    def _remap_schemalocs(self, pairs):
+        remapped = []
+
+        for ns, loc in pairs:
+            if ns in self.UPDATE_SCHEMALOC_MAP:
+                new_loc = self.UPDATE_SCHEMALOC_MAP[ns]
+            else:
+                new_loc = loc
+
+            remapped.append((ns, new_loc))
+
+        return remapped
+
+
+    def _update_schemalocs(self, root):
+        schemalocs = root.attrib.get(TAG_SCHEMALOCATION)
+        if not schemalocs:
+            return
+
+        l = schemalocs.split()
+        pairs = zip(l[::2], l[1::2])
+
+        cleaned = self._clean_schemalocs(pairs)
+        remapped = self._remap_schemalocs(cleaned)
+        updated = self._create_schemaloc_str(remapped)
+
+        root.attrib[TAG_SCHEMALOCATION] = updated
+
+
+    def _remap_namespaces(self, root):
+        remapped = {}
+        for alias, ns in root.nsmap.iteritems():
+            if ns in self.DISALLOWED_NAMESPACES:
+                continue
+
+            remapped[alias] = self.UPDATE_NS_MAP.get(ns, ns)
+
+        return remapped
+
+
+    def _update_namespaces(self, root):
+        """Updates the namespaces in the instance document to align with
+        with the updated schema. This will also remove any disallowed
+        namespaces if found in the instance document.
+
+        Returns:
+            A copy of the root document. It is impossible to update the
+            ``nsmap`` member of an etree._Element[Tree] directly, so we need
+            to make a copy with a modified initial nsmap.
+
+        """
+        remapped = self._remap_namespaces(root)
+        updated = etree.Element(root.tag, nsmap=remapped)
+        updated.attrib.update(root.attrib)
+        updated[:] = root[:]
+
+        return updated
 
 
 def _get_etree_root(doc):
