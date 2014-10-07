b786c8b8c5326d941c830972446ddb7e949fbc80
Bryan Worrell <bworrell@mitre.org>
2014-10-07 15:05:39 -0400

    Added STIX 1.0 unit tests

--- ramrod/test/stix/stix_1_0_test.py
+++ ramrod/test/stix/stix_1_0_test.py
@@ -6,9 +6,12 @@ from StringIO import StringIO
 
 import ramrod
 import ramrod.stix
+import ramrod.stix.stix_1_0
 import ramrod.utils as utils
+from ramrod.test import (_BaseVocab, _BaseDisallowed)
 
-UPDATER = ramrod.stix.STIX_1_0_Updater
+UPDATER_MOD = ramrod.stix.stix_1_0
+UPDATER = UPDATER_MOD.STIX_1_0_Updater
 
 class STIX_1_0_Test(unittest.TestCase):
     XML_VERSIONS = \
@@ -40,6 +43,135 @@ class STIX_1_0_Test(unittest.TestCase):
             updated_root = updated.document.getroot()
             updated_version = UPDATER.get_version(updated_root)
             self.assertEqual(version, updated_version)
+
+
+PACKAGE_TEMPLATE = \
+"""
+<stix:STIX_Package
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:stix="http://stix.mitre.org/stix-1"
+    xmlns:stixCommon="http://stix.mitre.org/common-1"
+    xmlns:indicator="http://stix.mitre.org/Indicator-2"
+    xmlns:ttp="http://stix.mitre.org/TTP-1"
+    xmlns:stixVocabs="http://stix.mitre.org/default_vocabularies-1"
+    xmlns:stix-capec="http://stix.mitre.org/extensions/AP#CAPEC2.5-1"
+    xmlns:stix-maec="http://stix.mitre.org/extensions/Malware#MAEC4.0-1"
+    xmlns:example="http://example.com/"
+    version="1.0">
+    %s
+</stix:STIX_Package>
+"""
+
+class MotivationVocab(_BaseVocab):
+    UPDATER = UPDATER_MOD.STIX_1_0_Updater
+    VOCAB_KLASS = UPDATER_MOD.MotivationVocab
+    VOCAB_COUNT = 1
+    VOCAB_XML = \
+    """
+    <stix:Indicators>
+        <stix:Indicator xsi:type="indicator:IndicatorType" version='2.0'>
+            <indicator:Type xsi:type="stixVocabs:MotivationVocab-1.0">Ideological - Anti-Establisment</indicator:Type>
+        </stix:Indicator>
+    </stix:Indicators>
+    """
+    XML = PACKAGE_TEMPLATE % (VOCAB_XML)
+
+
+class POSVocab(_BaseVocab):
+    UPDATER = UPDATER_MOD.STIX_1_0_Updater
+    VOCAB_KLASS = UPDATER_MOD.PlanningAndOperationalSupportVocab
+    VOCAB_COUNT = 1
+    VOCAB_XML = \
+    """
+    <stix:Indicators>
+        <stix:Indicator xsi:type="indicator:IndicatorType" version='2.0'>
+            <indicator:Type xsi:type="stixVocabs:PlanningAndOperationalSupportVocab-1.0">Planning </indicator:Type>
+        </stix:Indicator>
+    </stix:Indicators>
+    """
+    XML = PACKAGE_TEMPLATE % (VOCAB_XML)
+
+class DisallowedMAEC(_BaseDisallowed):
+    UPDATER = UPDATER_MOD.STIX_1_0_Updater
+    DISALLOWED_KLASS = UPDATER_MOD.DisallowedMAEC
+    DISALLOWED_COUNT = 1
+    DISALLOWED_XML = \
+    """
+    <stix:TTPs>
+        <stix:TTP xsi:type="ttp:TTPType">
+            <ttp:Behavior>
+                <ttp:Malware>
+                    <ttp:Malware_Instance xsi:type="stix-maec:MAEC4.0InstanceType">
+                        <stix-maec:MAEC/>
+                    </ttp:Malware_Instance>
+                </ttp:Malware>
+            </ttp:Behavior>
+        </stix:TTP>
+    </stix:TTPs>
+    """
+    XML = PACKAGE_TEMPLATE % (DISALLOWED_XML)
+
+
+class DisallowedMalware(_BaseDisallowed):
+    UPDATER = UPDATER_MOD.STIX_1_0_Updater
+    DISALLOWED_KLASS = UPDATER_MOD.DisallowedMalware
+    DISALLOWED_COUNT = 1
+    DISALLOWED_XML = \
+    """
+    <stix:TTPs>
+        <stix:TTP xsi:type="ttp:TTPType">
+            <ttp:Behavior>
+                <ttp:Malware>
+                    <ttp:Malware_Instance xsi:type="stix-maec:MAEC4.0InstanceType">
+                        <stix-maec:MAEC/>
+                    </ttp:Malware_Instance>
+                </ttp:Malware>
+            </ttp:Behavior>
+        </stix:TTP>
+    </stix:TTPs>
+    """
+    XML = PACKAGE_TEMPLATE % (DISALLOWED_XML)
+
+class DisallowedCAPEC(_BaseDisallowed):
+    UPDATER = UPDATER_MOD.STIX_1_0_Updater
+    DISALLOWED_KLASS = UPDATER_MOD.DisallowedCAPEC
+    DISALLOWED_COUNT = 1
+    DISALLOWED_XML = \
+    """
+    <stix:TTPs>
+        <stix:TTP xsi:type="ttp:TTPType">
+            <ttp:Behavior>
+                <ttp:Attack_Patterns>
+                    <ttp:Attack_Pattern xsi:type="stix-capec:CAPEC2.5InstanceType">
+                        <stix-capec:CAPEC Name="This cannot be translated" Status="Draft"/>
+                    </ttp:Attack_Pattern>
+                </ttp:Attack_Patterns>
+            </ttp:Behavior>
+        </stix:TTP>
+    </stix:TTPs>
+    """
+    XML = PACKAGE_TEMPLATE % (DISALLOWED_XML)
+
+
+class DisallowedAttackPatterns(_BaseDisallowed):
+    UPDATER = UPDATER_MOD.STIX_1_0_Updater
+    DISALLOWED_KLASS = UPDATER_MOD.DisallowedAttackPatterns
+    DISALLOWED_COUNT = 1
+    DISALLOWED_XML = \
+    """
+    <stix:TTPs>
+        <stix:TTP xsi:type="ttp:TTPType">
+            <ttp:Behavior>
+                <ttp:Attack_Patterns>
+                    <ttp:Attack_Pattern xsi:type="stix-capec:CAPEC2.5InstanceType">
+                        <stix-capec:CAPEC Name="This cannot be translated" Status="Draft"/>
+                    </ttp:Attack_Pattern>
+                </ttp:Attack_Patterns>
+            </ttp:Behavior>
+        </stix:TTP>
+    </stix:TTPs>
+    """
+    XML = PACKAGE_TEMPLATE % (DISALLOWED_XML)
 
 if __name__ == "__main__":
     unittest.main()
