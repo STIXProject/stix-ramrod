e41d002d24a91cb9713d37985ad38303f3796837
Bryan Worrell <bworrell@mitre.org>
2014-10-06 20:09:52 -0400

    Added 2.0-2.0.1 comma/list test

--- ramrod/test/cybox/cybox_2_0_test.py
+++ ramrod/test/cybox/cybox_2_0_test.py
@@ -41,5 +41,68 @@ class Cybox_2_0_Test(unittest.TestCase):
             self.assertEqual(version, updated_version)
 
 
+class CommaTest(unittest.TestCase):
+    UPDATER = ramrod.cybox.Cybox_2_0_Updater
+
+    ATTACKERS = [
+        'attacker@example.com',
+        'attacker1@example.com',
+        'attacker@bad.example.com'
+    ]
+
+    ESCAPED = "Et tu&comma; Brute?"
+    UNESCAPED = ESCAPED.replace("&comma;", ",")
+
+    NEW_DELIMITER = "##comma##"
+
+    XML_COMMAS = \
+    """
+    <cybox:Object xsi:type="EmailMessageObj:EmailMessageObjectType"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xmlns:cybox="http://cybox.mitre.org/cybox-2"
+        xmlns:EmailMessageObj="http://cybox.mitre.org/objects#EmailMessageObject-2"
+        xmlns:AddressObj="http://cybox.mitre.org/objects#AddressObject-2">
+        <cybox:Properties>
+            <EmailMessageObj:Header>
+                <EmailMessageObj:From category="e-mail">
+                    <AddressObj:Address_Value condition="Equals" apply_condition="ANY">%s</AddressObj:Address_Value>
+                </EmailMessageObj:From>
+                <EmailMessageObj:Subject condition="Equals"><![CDATA[%s]]></EmailMessageObj:Subject>
+            </EmailMessageObj:Header>
+        </cybox:Properties>
+    </cybox:Object>
+    """ % (",".join(ATTACKERS), ESCAPED)
+
+    @classmethod
+    def setUpClass(cls):
+        cls._xml = StringIO(cls.XML_COMMAS)
+
+    def test_lists(self):
+        nsmap = {'AddressObj': 'http://cybox.mitre.org/objects#AddressObject-2'}
+        updater = self.UPDATER()
+        root = utils.get_etree_root(self._xml)
+
+        updater._update_lists(root)
+        address_value = root.xpath('.//AddressObj:Address_Value', namespaces=nsmap)[0].text
+        self.assertEqual(address_value, self.NEW_DELIMITER.join(self.ATTACKERS))
+
+    def test_commas(self):
+        nsmap = {'EmailMessageObj': 'http://cybox.mitre.org/objects#EmailMessageObject-2'}
+        updater = self.UPDATER()
+        root = utils.get_etree_root(self._xml)
+
+        updater._update_lists(root)
+        subject = root.xpath('.//EmailMessageObj:Subject', namespaces=nsmap)[0].text
+        self.assertEqual(subject, self.UNESCAPED)
+
+
+
+class OptionalsTest(unittest.TestCase):
+    UPDATER = ramrod.cybox.Cybox_2_0_Updater
+
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
