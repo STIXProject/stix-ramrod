de4a109d40995de53d3f61f53c3b133391122918
Bryan Worrell <bworrell@mitre.org>
2014-10-12 00:23:14 -0400

    Removed unused import

--- ramrod/__init__.py
+++ ramrod/__init__.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2014, The MITRE Corporation. All rights reserved.
 # See LICENSE.txt for complete terms.
 
-import itertools
 from StringIO import StringIO
 from distutils.version import StrictVersion
 from collections import defaultdict
